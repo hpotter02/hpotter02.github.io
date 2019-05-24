22a8f77b4eb5b28e060f4b53cc2f1fcead965a7c
dereisl <32849066+dereisl@users.noreply.github.com>
2019-05-24 15:09:24 +0200

    Responsive size of OTTER & strict mode

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 if (location.protocol === "http:") location.replace(/^http:/, "https:");
 
 const otter = document.getElementById("otter");
@@ -5,3 +7,16 @@ const otter = document.getElementById("otter");
 addEventListener("deviceorientation", (e) => {
 	otter.style.transform = "rotate(" + (- e.gamma) + "deg)";
 });
+
+function resizeOtter() {
+	if (innerHeight > innerWidth) {
+		otter.style.width = "100vw";
+		otter.style.height = "50vw";
+	} else {
+		otter.style.width = "100vh";
+		otter.style.height = "50vh";
+	}
+}
+
+addEventListener("resize", resizeOtter);
+resizeOtter();
