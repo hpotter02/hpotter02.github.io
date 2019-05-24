ada93620ee2a439cc115a6771f37efa136ceb5e3
dereisl <32849066+dereisl@users.noreply.github.com>
2019-05-24 22:21:18 +0200

    Add rotation of otter using mouse

--- script.js
+++ script.js
@@ -4,8 +4,29 @@ if (location.protocol === "http:") location.replace(/^http:/, "https:");
 
 const otter = document.getElementById("otter");
 
+function rotateOtter(angle) {
+	otter.style.transform = "rotate(" + (angle) + "deg)";
+}
+
+function processMouseMove(e) {
+	const xFromCenter = innerWidth / 2 - e.clientX;
+	const yFromCenter = innerHeight / 2 - e.clientY;
+	let angle = Math.atan(yFromCenter / xFromCenter) * 180 / Math.PI;
+	if (xFromCenter > 0) angle += 180;
+	// Add 20 degrees, so that the otter looks directly at your mouse pointer:
+	angle += (angle <= 340) ? 20 : -340;
+	rotateOtter(angle);
+}
+
+addEventListener("mousemove", processMouseMove);
+
+let isMoveHandlerRemoved = false;
 addEventListener("deviceorientation", (e) => {
-	otter.style.transform = "rotate(" + (- e.gamma) + "deg)";
+	if (!isMoveHandlerRemoved) {
+		removeEventListener("mousemove", processMouseMove);
+		isMoveHandlerRemoved = true;
+	}
+	rotateOtter(-e.gamma);
 });
 
 function resizeOtter() {
